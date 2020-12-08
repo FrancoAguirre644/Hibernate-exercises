f567f548d819d7e5f1eac22d84e9d6cca64ddc87
Franco <FrancoAguirre644@gmail.com>
2020-12-08 17:40:43 -0300

    Set de pruebas ejecutado con exito (Sistema YPF)

--- SistemaYPF/src/negocio/CompraABM.java
+++ SistemaYPF/src/negocio/CompraABM.java
@@ -1,6 +1,7 @@
 package negocio;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import dao.CompraDao;
@@ -30,6 +31,36 @@ public class CompraABM {
 
 	public List<Compra> traer(Cliente cliente) {
 		return CompraDao.getInstanciaCompraDao().traer(cliente);
+	}
+
+	public List<Compra> traer(LocalDate desde, LocalDate hasta) {
+		return CompraDao.getInstanciaCompraDao().traer(desde, hasta);
+	}
+
+	public List<Compra> traer(Producto producto) {
+		return CompraDao.getInstanciaCompraDao().traer(producto);
+	}
+
+	public List<Compra> traer(LocalDate desde, LocalDate hasta, Cliente cliente) {
+		List<Compra> compras = new ArrayList<Compra>();
+
+		for (Compra c : this.traer(desde, hasta)) {
+			if (c.getTarjeta().getCliente().getIdCliente() == cliente.getIdCliente())
+				compras.add(c);
+		}
+
+		return compras;
+	}
+
+	public List<Compra> traer(LocalDate desde, LocalDate hasta, Producto producto) {
+		List<Compra> compras = new ArrayList<Compra>();
+
+		for (Compra c : this.traer(desde, hasta)) {
+			if (c.getProducto().getIdProducto() == producto.getIdProducto())
+				compras.add(c);
+		}
+
+		return compras;
 	}
 
 	public int agregar(LocalDate fecha, Producto producto, double cantidad, Cliente cliente) {
--- SistemaYPF/src/test/test.java
+++ SistemaYPF/src/test/test.java
@@ -2,7 +2,6 @@ package test;
 
 import java.time.LocalDate;
 
-import dao.TarjetaDao;
 import negocio.ClienteABM;
 import negocio.CompraABM;
 import negocio.ProductoABM;
@@ -12,55 +11,224 @@ public class test {
 
 	public static void main(String[] args) {
 
-//		// ClienteABM.getInstanciaClienteABM().agregar("Micaela", "Hidalgo",
-//		// "mhidalgo@gmail.com", 11111111);
-//
-//		// ProductoABM.getInstanciaProductoABM().agregar("NAFTA SUPER", 12.0, "litros",
-//		// 6.0);
-//
-//		ClienteABM.getInstanciaClienteABM().traer().stream().forEach((c) -> {
-//			System.out.println(c);
-//		});
-//
-//		ProductoABM.getInstanciaProductoABM().traer().stream().forEach((p) -> {
-//			System.out.println(p);
-//		});
-//
-//		System.out.println(ClienteABM.getInstanciaClienteABM().traer((long) 11111111));
-//		System.out.println();
-//		System.out.println(ProductoABM.getInstanciaProductoABM().traer("NAFTA SUPER"));
-//
-//		// TarjetaABM.getInstanciaTarjetaABM().agregar(ClienteABM.getInstanciaClienteABM().traer(1));
-//
-//		System.out.println(TarjetaABM.getInstanciaTarjetaABM().traer());
-//
-//		System.out.println();
-//
-//		System.out.println();
-//
-//		System.out.println(TarjetaABM.getInstanciaTarjetaABM().traer(ClienteABM.getInstanciaClienteABM().traer(1)));
-//
-//		// System.out.println(CompraABM.getInstanciaCompraABM().agregar(LocalDate.now(),
-//		// ProductoABM.getInstanciaProductoABM().traer("NAFTA SUPER"), 20,
-//		// ClienteABM.getInstanciaClienteABM().traer(1)));
-//
-//		System.out.println();
-//
-//		System.out.println(CompraABM.getInstanciaCompraABM().traer());
-//
-//		System.out.println();
-//		System.out.println();
-//
-//		System.out.println(CompraABM.getInstanciaCompraABM().traer(ClienteABM.getInstanciaClienteABM().traer(1)));
-//
-//		System.out.println();
-//
-//		System.out.println(CompraABM.getInstanciaCompraABM()
-//				.traerCantidadVendida(ProductoABM.getInstanciaProductoABM().traer("NAFTA SUPER")));
-//		
-//		System.out.println();
-//		
-//		System.out.println(CompraABM.getInstanciaCompraABM().traerTotalVentas(ProductoABM.getInstanciaProductoABM().traer("NAFTA SUPER")));
+		System.out.println(
+				"-------------------------------------------Punto-1------------------------------------------------");
+
+		try {
+			ClienteABM.getInstanciaClienteABM().agregar("Micaela", "Hidalgo", "mhidalgo@gmail.com", 11111111);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			ClienteABM.getInstanciaClienteABM().agregar("Juan", "Lopez", "jlopez@gmail.com", 22222222);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			ClienteABM.getInstanciaClienteABM().agregar("Daniela", "Rojas", "drojas@gmail.com", 33333333);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			ClienteABM.getInstanciaClienteABM().agregar("Lucas", "Suarez", "lsuarez@gmail.com", 44444444);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			ClienteABM.getInstanciaClienteABM().agregar("Juan", "Lopez", "jlopez@gmail.com", 22222222);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		ClienteABM.getInstanciaClienteABM().traer().forEach((c) -> {
+			System.out.println(c);
+		});
+
+		System.out.println(
+				"-------------------------------------------Punto-2------------------------------------------------");
+
+		try {
+			ProductoABM.getInstanciaProductoABM().agregar("NAFTA SUPER", 12.0, "litros", 6.0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			ProductoABM.getInstanciaProductoABM().agregar("NAFTA INFINIA", 15.0, "litros", 3.0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			ProductoABM.getInstanciaProductoABM().agregar("INFINIA DIESEL", 15.5, "litros", 3.0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			ProductoABM.getInstanciaProductoABM().agregar("GNC", 10.0, "litros", 1.0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			ProductoABM.getInstanciaProductoABM().agregar("NAFTA INFINIA", 15.0, "litros", 3.0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		ProductoABM.getInstanciaProductoABM().traer().stream().forEach((p) -> {
+			System.out.println(p);
+		});
+
+		System.out.println(
+				"-------------------------------------------Punto-3------------------------------------------------");
+
+		try {
+			TarjetaABM.getInstanciaTarjetaABM().agregar(ClienteABM.getInstanciaClienteABM().traer(1));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			TarjetaABM.getInstanciaTarjetaABM().agregar(ClienteABM.getInstanciaClienteABM().traer(2));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			TarjetaABM.getInstanciaTarjetaABM().agregar(ClienteABM.getInstanciaClienteABM().traer(3));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			TarjetaABM.getInstanciaTarjetaABM().agregar(ClienteABM.getInstanciaClienteABM().traer(4));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		TarjetaABM.getInstanciaTarjetaABM().traer().forEach((t) -> {
+			System.out.println(t);
+		});
+
+		try {
+			TarjetaABM.getInstanciaTarjetaABM().agregar(ClienteABM.getInstanciaClienteABM().traer(3));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println(
+				"-------------------------------------------Punto-4------------------------------------------------");
+
+		CompraABM.getInstanciaCompraABM().agregar(LocalDate.of(2017, 10, 17),
+				ProductoABM.getInstanciaProductoABM().traer("NAFTA SUPER"), 10.0,
+				ClienteABM.getInstanciaClienteABM().traer(1));
+
+		CompraABM.getInstanciaCompraABM().agregar(LocalDate.of(2017, 10, 17),
+				ProductoABM.getInstanciaProductoABM().traer("INFINIA DIESEL"), 10.5,
+				ClienteABM.getInstanciaClienteABM().traer(3));
+
+		CompraABM.getInstanciaCompraABM().agregar(LocalDate.of(2017, 10, 17),
+				ProductoABM.getInstanciaProductoABM().traer("NAFTA INFINIA"), 12.0,
+				ClienteABM.getInstanciaClienteABM().traer(2));
+
+		CompraABM.getInstanciaCompraABM().agregar(LocalDate.of(2017, 10, 17),
+				ProductoABM.getInstanciaProductoABM().traer("NAFTA SUPER"), 10.0,
+				ClienteABM.getInstanciaClienteABM().traer(1));
+
+		CompraABM.getInstanciaCompraABM().agregar(LocalDate.of(2017, 10, 18),
+				ProductoABM.getInstanciaProductoABM().traer("NAFTA SUPER"), 5.0,
+				ClienteABM.getInstanciaClienteABM().traer(1));
+
+		CompraABM.getInstanciaCompraABM().agregar(LocalDate.of(2017, 10, 18),
+				ProductoABM.getInstanciaProductoABM().traer("INFINIA DIESEL"), 7.5,
+				ClienteABM.getInstanciaClienteABM().traer(3));
+
+		CompraABM.getInstanciaCompraABM().agregar(LocalDate.of(2017, 10, 19),
+				ProductoABM.getInstanciaProductoABM().traer("NAFTA SUPER"), 8.0,
+				ClienteABM.getInstanciaClienteABM().traer(1));
+
+		CompraABM.getInstanciaCompraABM().agregar(LocalDate.of(2017, 10, 19),
+				ProductoABM.getInstanciaProductoABM().traer("INFINIA DIESEL"), 22.0,
+				ClienteABM.getInstanciaClienteABM().traer(3));
+
+		CompraABM.getInstanciaCompraABM().agregar(LocalDate.of(2017, 10, 20),
+				ProductoABM.getInstanciaProductoABM().traer("NAFTA INFINIA"), 25.0,
+				ClienteABM.getInstanciaClienteABM().traer(2));
+
+		CompraABM.getInstanciaCompraABM().agregar(LocalDate.of(2017, 10, 20),
+				ProductoABM.getInstanciaProductoABM().traer("NAFTA INFINIA"), 20.5,
+				ClienteABM.getInstanciaClienteABM().traer(2));
+
+		CompraABM.getInstanciaCompraABM().traer().stream().forEach((c) -> {
+			System.out.println(c);
+		});
+
+		System.out.println(
+				"-------------------------------------------Punto-5------------------------------------------------");
+
+		CompraABM.getInstanciaCompraABM().traer(ClienteABM.getInstanciaClienteABM().traer(3)).forEach((c) -> {
+			System.out.println(c);
+		});
+
+		System.out.println(
+				"-------------------------------------------Punto-6------------------------------------------------");
+
+		CompraABM.getInstanciaCompraABM().traer(LocalDate.of(2017, 10, 18), LocalDate.of(2017, 10, 20)).forEach((c) -> {
+			System.out.println(c);
+		});
+
+		System.out.println(
+				"-------------------------------------------Punto-7------------------------------------------------");
+
+		CompraABM.getInstanciaCompraABM().traer(LocalDate.of(2017, 10, 18), LocalDate.of(2017, 10, 20),
+				ClienteABM.getInstanciaClienteABM().traer(3));
+
+		System.out.println(
+				"-------------------------------------------Punto-8------------------------------------------------");
+
+		CompraABM.getInstanciaCompraABM().traer(ProductoABM.getInstanciaProductoABM().traer("INFINIA DIESEL"))
+				.forEach((c) -> {
+					System.out.println(c);
+				});
+
+		System.out.println(
+				"-------------------------------------------Punto-9------------------------------------------------");
+
+		CompraABM.getInstanciaCompraABM().traer(LocalDate.of(2017, 10, 18), LocalDate.of(2017, 10, 20),
+				ProductoABM.getInstanciaProductoABM().traer("INFINIA DIESEL"));
+
+		System.out.println(
+				"-------------------------------------------Punto-10------------------------------------------------");
+
+		System.out.println(CompraABM.getInstanciaCompraABM()
+				.traerCantidadVendida(ProductoABM.getInstanciaProductoABM().traer("INFINIA DIESEL")));
+
+		System.out.println(
+				"-------------------------------------------Punto-11------------------------------------------------");
+
+		System.out.println(CompraABM.getInstanciaCompraABM()
+				.traerTotalVentas(ProductoABM.getInstanciaProductoABM().traer("INFINIA DIESEL")));
 
 	}
 
